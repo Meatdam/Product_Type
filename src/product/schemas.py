d0e201c0ddd617b17a14fc5848eac2234b85c4c0
Kuzkin <heartsong_@mail.ru>
2024-09-13 14:32:38 +0500

    add file src/product schemas.py

---
+++ src/product/schemas.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel
+
+
+class ProductNameSchema(BaseModel):
+    """Product name schema."""
+    name: str
+    id: int
+
+
+class BaseProductSchema(BaseModel):
+    """Base product schema."""
+    id: int
+    name: str
+    product_type_id: int
+
+
+class CreateProductSchema(BaseModel):
+    """Product creation scheme."""
+    name: str
+    product_type_id: int
+
+
+class ProductListSchema(BaseProductSchema):
+    """Product list scheme."""
+
+
+from src.product_type.shemas import TypeProductSchema
+
+
+class ProductDetailSchema(BaseProductSchema):
+    """Product detail scheme."""
+    product_type: TypeProductSchema | None = None
